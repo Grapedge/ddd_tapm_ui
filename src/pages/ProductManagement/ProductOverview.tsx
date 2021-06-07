34e3557ada99fd4455bd7a648713a49c1f76b490
grapedge <grapedge@gmail.com>
2021-06-07 18:43:28 +0800

    feat: 增加概览自动换行

--- src/pages/ProductManagement/ProductOverview.tsx
+++ src/pages/ProductManagement/ProductOverview.tsx
@@ -9,21 +9,21 @@ const ProductOverview: React.FC = () => {
       <PageHeader title="项目概览" />
       <div className={classes.container}>
         <Row gutter={16}>
-          <Col span={6}>
+          <Col flex={1} className={classes.col}>
             {/* 当前时间 */}
             <CurTimeCard />
           </Col>
-          <Col span={6}>
+          <Col flex={1} className={classes.col}>
             <Card>
               <Statistic title="代码仓库" value={20} suffix="个" />
             </Card>
           </Col>
-          <Col span={6}>
+          <Col flex={1} className={classes.col}>
             <Card>
               <Statistic title="待办事项" value={20} suffix="个" />
             </Card>
           </Col>
-          <Col span={6}>
+          <Col flex={1} className={classes.col}>
             <Card>
               <Statistic title="冲刺数" value={15} suffix="个" />
             </Card>
